bce5cdf6cfb7c4ff0913884c5a936bb766010f67
AmIWatchU <user106512@gmail.com>
2021-04-05 11:21:10 +0300

    Add project files.

---
+++ Lab5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Write any word:");
+			string sc = Console.ReadLine();
+			Console.Write("Matches with last letter -->  ");
+			char ch = sc[sc.Length - 1];
+			bool flag = true;
+			for (int i = 0; i < sc.Length - 1; i++)
+			{
+				if (ch == sc[i])
+				{
+					Console.Write(i + 1 + " ");
+					flag = false;
+			
+				}
+			}
+			if (flag)
+			{
+				Console.WriteLine("No matches");
+			}
+			Console.ReadKey();
+		}
+	}
+}
